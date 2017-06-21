b4022373098d79f2abbf822936ef61dfe8332910
Dolphin Oracle <dolphinoracle@gmail.com>
2017-06-20 21:12:49 -0400

    hack for ctrl+alt+backspace

--- kbd.sh
+++ kbd.sh
@@ -2,5 +2,13 @@
 
 setxkbmap -layout 
 
+#set terminate key unless already set
+
+setxkbmap -query |grep -q terminate
+
+if [ $? != 0 ]; then
+setxkbmap -option terminate:ctrl_alt_bksp
+fi
+
 exit
  
